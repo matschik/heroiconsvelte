5f6f5dc014e93e8b526bfc41aac65e1eaadc28d1
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-09-02 02:47:11 +0200

    use optimized heroicons svgs

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
 import pascalcase from 'pascalcase';
 
-const BASE_DIR = 'node_modules/heroicons/src';
+const BASE_DIR = 'node_modules/heroicons/optimized';
 
 const svgIconPaths = await glob([`${BASE_DIR}/**/*.svg`]);
 
